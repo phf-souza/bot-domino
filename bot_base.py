ebee649610bf86a1985becc1fcc3537591689c8c
phf.souza <pedro.fontourasouza@gmail.com>
2026-06-22 14:43:19 -0300

    Core HFs implemented

--- bot_base.py
+++ bot_base.py
@@ -16,29 +16,6 @@ def joga(estado):
 
 
 # ===========================
-# HEURISTIC FUNCTIONS
-# ===========================
-def hfHandBalancing(move,gameData):
-    return 0
-
-def hfBoardControl(move,gameData):
-    return 0
-
-def hfDumpDoubles(move,gameData):
-    return 0
-
-def hfMinimizeHandWeight(move,gameData):
-    return 0
-
-hfFunctions = [
-    hfHandBalancing,
-    hfBoardControl,
-    hfDumpDoubles,
-    hfMinimizeHandWeight
-]
-
-
-# ===========================
 # DEGENERATE CASES
 # ===========================
 def countMoves(validMoves):         
@@ -59,6 +36,53 @@ def onlyMove(validMoves):
 
 
 # ===========================
+# HEURISTIC FUNCTIONS
+# ===========================
+def hfHandBalancing(move,gameData):
+    resultedSuits = suitsAfterMove(move, gameData["currentSuits"])
+    qualityValue = 7 - resultedSuits.count(0) 
+    
+    return stdNormalize(qualityValue, 0, 7)
+
+def hfBoardControl(move,gameData):
+    resultedSuits = suitsAfterMove(move, gameData["currentSuits"])
+    leftEnd, rightEnd = gameData["boardEnds"]
+    tile, side = move
+
+    if side == "esquerda":
+        newLeftEnd = tile[1] if tile[0] == leftEnd else tile[0]
+        qualityValue = resultedSuits[newLeftEnd] + resultedSuits[rightEnd]
+    else:
+        newRightEnd = tile[1] if tile[0] == rightEnd else tile[0]
+        qualityValue = resultedSuits[leftEnd] + resultedSuits[newRightEnd]
+
+    return stdNormalize(qualityValue, 0, 9)
+
+def hfDumpDoubles(move,gameData):
+    if move[0][0] == move[0][1]:
+        return 1
+    return 0
+
+def hfMinimizeHandWeight(move,gameData):
+    qualityValue = move[0][0] + move[0][1]
+    
+    return stdNormalize(qualityValue, 0, 12)
+
+def hfCloseGame(move, gameData):
+    if gameData["handSize"] == 1:
+        return 1
+    return 0
+
+hfFunctions = [
+    hfHandBalancing,
+    hfBoardControl,
+    hfDumpDoubles,
+    hfMinimizeHandWeight,
+    hfCloseGame
+]
+
+
+# ===========================
 # CONDITIONAL WEIGHT FUNCTION
 # ===========================
 def weightFunction(gameState):
@@ -69,11 +93,10 @@ def weightFunction(gameState):
 # QUALITY MOVE CALCULATOR
 # ===========================
 def bestMove(gameState):
-    availableMoves = turnMovesInArray(gameState["movimentos_validos"])
-    currentWeight = weightFunction(gameState)
-    gameData = gameState
+    gameData = gameStateToData(gameState)
+    currentWeight = weightFunction(gameData["gameState"])
 
-    bestMoveTuple = max(availableMoves, key=lambda move: hfSum(move, currentWeight, gameData))
+    bestMoveTuple = max(gameData["availableMoves"], key=lambda move: hfSum(move, currentWeight, gameData))
 
     return {
         "jogada": "joga",
@@ -98,3 +121,41 @@ def hfSum(move, weightList, gameData):
         qualityValue += weightList[index]*hf(move, gameData)
     
     return qualityValue
+
+
+# ===========================
+# DATA CLEANING
+# ===========================
+def suitsAfterMove(move, currentSuits):
+    expectedSuits = currentSuits.copy()
+    expectedSuits[move[0][0]] -= 1
+    expectedSuits[move[0][1]] -= 1
+
+    return expectedSuits
+
+def stdNormalize(value, min, max):
+    return (value-min)/(max-min)
+
+def gameStateToData(gameState):
+    hand = gameState["mao"]
+
+    return {
+        "gameState": gameState,
+        "availableMoves": turnMovesInArray(gameState["movimentos_validos"]),
+        "playerHand": hand,
+        "currentSuits": countSuits(hand),
+        "handSize": len(hand),
+        "boardEnds": (gameState["esquerda_end"], gameState["direita_end"])
+    }
+
+# ===========================
+# CARD COUNTING
+# ===========================
+def countSuits(hand):
+    numSuits = [0]*7
+
+    for tile in hand:
+        numSuits[tile[0]] += 1
+        numSuits[tile[1]] += 1
+    
+    return numSuits
