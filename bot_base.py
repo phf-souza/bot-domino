a5de54cb3d4446b50253d829b3063051979340a9
phf-souza <pedro.fontourasouza@gmail.com>
2026-06-22 10:37:52 -0300

    Progress 2626.06.21

---
+++ bot_base.py
@@ -0,0 +1,19 @@
+def joga(estado):
+
+
+
+    if  == 0:
+        return {
+            " jogada ": " passa "
+        }
+    elif 
+
+
+    return bestMove(estado)
+
+def hasMoves(validMoves):
+    if validMoves["esquerda"] + validMoves["direita"] == 0:
+        return False
+    
+    return True
+
